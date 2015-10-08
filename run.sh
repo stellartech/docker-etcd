1049336d8464c8c83e45e64ac4ff6e639ff6e57e
Andy Kirkham <andy@spiders-lair.com>
2015-10-08 02:17:58 -0700

    Add 2379 listener

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ else
     ip=`ifconfig | awk '/inet addr/{print substr($2,6)}' | head -1`
 fi
 
-/usr/local/bin/etcd --data-dir=/var/etcd -listen-peer-urls "http://$ip:4001"
+/usr/local/bin/etcd --data-dir=/var/etcd -listen-peer-urls "http://$ip:4001,http://$ip:2379"
