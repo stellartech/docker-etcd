aef4848fb00fa100c217f00b01c36a945d91b77a
Andy Kirkham <andy@spiders-lair.com>
2015-10-08 05:16:50 -0700

    wip

--- run.sh
+++ run.sh
@@ -14,5 +14,11 @@ fi
 /usr/local/bin/etcd \
 	--data-dir=/var/etcd \
 	-name etcd0 \
-	-listen-client-urls "http://0.0.0.0:2379" \
-	-listen-peer-urls http://0.0.0.0:2380 	
+	-advertise-client-urls http://${ip}:2379,http://${ip}:4001 \
+	-listen-client-urls "http://0.0.0.0:2379,http://0.0.0.0:4001" \
+	-initial-advertise-peer-urls http://${ip}:2380 \
+	-listen-peer-urls http://0.0.0.0:2380 \
+	-initial-cluster-token etcd-cluster-1 \
+	-initial-cluster etcd0=http://${ip}:2380 \
+	-initial-cluster-state new
+
